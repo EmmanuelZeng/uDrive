3b775059eab5cdff9cafbc7fa29dea5ab3f615e1
EmmanuelZeng <emmazeng38@gmail.com>
2025-01-18 18:08:38 +0100

    Final V1_app

--- app/Http/Controllers/SharedFilesController.php
+++ app/Http/Controllers/SharedFilesController.php
@@ -13,7 +13,7 @@ class SharedFilesController extends Controller
     public function index(Request $request, File $file){
         $user = auth()->user();
         $users = User::where('id', '!=', $user->id)
-                 ->where('role_id', '!=', 2)
+                 ->where('role_id', '!=', 1)
                  ->get();
         $folders = $user->folders()->get();
         //$files = Share::where('user_id', auth()->id())->with('file')->get();
